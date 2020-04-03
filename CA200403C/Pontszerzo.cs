f410f850f32dd7fb71589c45a8f6983c28a2ade0
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-04-03 12:50:42 +0200

    Add project files.

---
+++ CA200403C/Pontszerzo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200403C
+{
+    class Pontszerzo
+    {
+        public int Helyezes { get; set; }
+        public int SportolokSzama { get; set; }
+        public string Sportag { get; set; }
+        public string Versenyszam { get; set; }
+        public int OlimpiaiPont => Helyezes == 1 ? 7 : (7 - Helyezes);
+        public Pontszerzo(string s)
+        {
+            var t = s.Split(' ');
+
+            Helyezes = int.Parse(t[0]);
+            SportolokSzama = int.Parse(t[1]);
+            Sportag = t[2];
+            Versenyszam = t[3];
+        }
+    }
+}
---
+++ CA200403C/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200403C
+{
+    class Program
+    {
+        static List<Pontszerzo> pontszerzok;
+        static void Main(string[] args)
+        {
+            F02();
+            F03();
+            F04();
+            F05();
+            F06();
+            F07();
+            F08();
+            Console.ReadKey();
+        }
+
+        private static void F05()
+        {
+            var sum = pontszerzok.Sum(x => x.OlimpiaiPont);
+            Console.WriteLine($"F5: {sum}");
+        }
+
+        private static void F08()
+        {
+            var r = pontszerzok
+                .OrderByDescending(x => x.SportolokSzama)
+                .ToList()
+                .First();
+
+            Console.WriteLine($"F8: {r.Helyezes} {r.SportolokSzama} {r.Sportag} {r.Versenyszam}");
+        }
+
+        private static void F07()
+        {
+            var sw = new StreamWriter(@"..\..\Res\helsinki2.txt");
+
+            pontszerzok.ForEach(x => sw.WriteLine(
+                $"{x.Helyezes} {x.SportolokSzama} {x.OlimpiaiPont} " +
+                $"{(x.Sportag == "kajakkenu" ? "kajak-kenu" : x.Sportag)} {x.Versenyszam}"));
+
+            sw.Close();
+        }
+
+        private static void F06()
+        {
+            var r = pontszerzok
+                .Where(x => x.Sportag == "uszas" || x.Sportag == "torna")
+                .GroupBy(x => x.Sportag)
+                .OrderBy(x => x.Count())
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            Console.WriteLine($"F6: {(r["torna"] == r["uszas"] ? "egyenlo" : r.Last().Key)}");
+        }
+
+        private static void F04()
+        {
+            var r = pontszerzok.GroupBy(x => x.Helyezes)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            Console.WriteLine($"F4: a:{r[1]} - e:{r[2]} - b:{r[3]}");
+        }
+
+        private static void F03()
+        {
+            Console.WriteLine($"F3: {pontszerzok.Count}");
+        }
+
+        private static void F02()
+        {
+            pontszerzok = new List<Pontszerzo>();
+            var sr = new StreamReader(@"..\..\Res\helsinki.txt", Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                pontszerzok.Add(new Pontszerzo(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
